d2b38a5e2dd3ae06146cf28c87728f8ecc96a393
kseidl <kseidl@student.tgm.ac.at>
2021-04-12 14:50:13 +0200

    statische Funktionen

--- php/seidl_eisenkirchner_statische Methoden.php
+++ php/seidl_eisenkirchner_statische Methoden.php
@@ -1,4 +1,21 @@
 <?php
 //Static -> statische Methoden und EIgenschaften -> gelten global und sind nicht an einzelnes objekt gekoppelt
+//Klassenname :: methodenname -> Aufruf
 
 
+//KLasse die ein A mit angegebenen hs schreitb
+class Ah {
+    public $a = 'A';
+
+    public static function ha_s($zahl){
+        for($i = 0; $i < $zahl; $i ++){
+            $a += 'h';
+        }
+        $a += '!';
+        return $a;
+}
+
+
+}
+//Sollte nun das A ausgeben
+echo Ah :: ha_s(3);
