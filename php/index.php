4ed6058adf1564999acb0ea58fb8e1ef69dc5961
kseidl <kseidl@student.tgm.ac.at>
2021-04-19 14:00:34 +0200

    eingabe ermöglichen

--- php/index.php
+++ php/index.php
@@ -1,105 +1,53 @@
-<?php
-class Kunde
-{
-    //definition von attributen
-    private $vname;
-    private $nname;
-    private $kid;
-    private $email;
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Gästebuch</title>
+    <link href="../css/Bootstrap/bootstrap.css" rel="stylesheet">
+</head>
 
-    /**
-     * Kunde constructor.
-     * @param $vname
-     * @param $nname
-     * @param $kid
-     * @param $email
-     */
-    public function __construct($vname, $nname, $kid, $email)
-    {
-
-        //Eigenschaft oder Methode innerhalb von klasse abrufen -> mit $this
-        $this->vname = $vname;
-        $this->nname = $nname;
-        $this->kid = $kid;
-        $this->email = $email;
-    }
-
-    /**
-     * @return den vornamen
-     */
-    public function getVname()
-    {
-        return $this->vname;
-    }
-
-    /**
-     * @param $vname übernimmt vornamen
-     */
-    public function setVname($vname)
-    {
-        $this->vname = $vname;
-    }
-
-    /**
-     * @return den nachnamen
-     */
-    public function getNname()
-    {
-        return $this->nname;
-    }
-
-    /**
-     * @param  $nname übernimmt nachnamen
-     */
-    public function setNname($nname)
-    {
-        $this->nname = $nname;
-    }
-
-    /**
-     * @return die ID
-     */
-    public function getKid()
-    {
-        return $this->kid;
-    }
-
-    /**
-     * @param $kid setzt ID
-     */
-    public function setKid($kid)
-    {
-        $this->kid = $kid;
-    }
-
-    /**
-     * @return die Email
-     */
-    public function getEmail()
-    {
-        return $this->email;
-    }
-
-    /**
-     * @param $email setzt email
-     */
-    public function setEmail($email)
-    {
-        $this->email = $email;
-    }
+<body>
+<div class="container-fluid">
+    <div class="col-sm-1">
+        <h1>Kundenformular</h1>
+    </div>
+    <!-- Daten eingegeben -->
+    <form action="indexBeispiel.php" method="POST" enctype="multipart/form-data">
+        <div class="form-group">
+            <label class="col-sm-2">Vorname:</label>
+            <div class="col-sm-10">
+                <input type="text" name="vname" />
+            </div>
 
 
-}
+        </div>
+        <div class="form-group">
+            <label class="col-sm-2">Nachname:</label>
+            <div class="col-sm-10"><input type="text" name="nname" />
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label class="col-sm-2">Email:</label>
+            <div class="col-sm-10"><input type="email" name="email" />
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label class="col-sm-2">ID:</label>
+            <div class="col-sm-10"><input type="number" name="kid" />
+            </div>
+        </div>
+
+
+        <input type="submit"/>
+
+    </form>
 
 
 
-//Referenzieren in PHP
+</div>
 
-//-new- => neue Objekte erzeugt werden
-$sam = new Kunde('Sam', 'Müller', 1, 'sam@müller.at');
-
-//durch Pfeile -> auf Eigenschaften der Objekte zugreifen
-echo "ID: $sam->getKid \nVorname:$sam->getVname \nNachname: $sam->getNname \nEmail:$sam->getEmail";
-
-
-?>
+</body>
+</html>
